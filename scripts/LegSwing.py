b93c72147d6656d2617d4c8a2f7b9fcd2df097d2
Ryan <ryan.leonard71@gmail.com>
2015-10-21 21:34:00 -0700

    Finished the "Leg Swing" animation polishing

--- scripts/LegSwing.py
+++ scripts/LegSwing.py
@@ -1,28 +1,56 @@
 #!/bin/python
+# Assume that if we find a "body" then it is a lego body and it will have the children we expect of it
 import maya.cmds as mc
+import re
 
 
-def kick(maya_object, animation_time=24):
-    mc.select(maya_object)
-    # Get the starting time and declare any "parameters" inline for now
-    starting_time = mc.currentTime(query=True)
-    swing_back_time = 0.2 * animation_time + starting_time
-    swing_kick_time = 0.15 * animation_time + swing_back_time
-    ending_time = animation_time + starting_time
+def kick(drama=1.5, animation_t=24):
+    # Figure out the paths to the objects we will be interacting with
+    selection = mc.ls(selection=True)
+    lego_body = ''
+    for item in selection:
+        match = re.search("(body.*)\|*?", item)
+        if match:
+            lego_body = match.group(1)
+    if lego_body is '':
+        return """Was unable to perform kick since we could not find a "lego body" """
+    left_arm = lego_body+"|arm_L"
+    right_arm = lego_body+"|arm_R"
+    right_leg = lego_body+"|hips|leg_R"
+    # Animation Variables
+    body_rotation = -10 * drama
+    body_sway = 10 * drama
+    leg_back_swing = 35 * drama
+    arm_back_swing = -30 * drama
+    arm_swing = 20 * drama
+    leg_swing = -50 * drama
+    start_time = mc.currentTime(query=True)
 
     # Set initial Keyframe
-    mc.setKeyframe(at="rotateX", time=starting_time)
+    current_time = start_time
+    mc.setKeyframe(left_arm, right_arm, right_leg, lego_body, at="rotateX", v=0, t=current_time)
+    mc.setKeyframe(lego_body, at="rotateY", v=0, t=current_time)
 
     # Set keyframe swinging leg back
-    mc.rotate(30, 0, 0, r=True)
-    mc.setKeyframe(at="rotateX", time=swing_back_time)
+    current_time = start_time + (0.2*animation_t)
+    mc.setKeyframe(left_arm, at="rotateX", v=arm_back_swing, t=current_time)
+    mc.setKeyframe(right_arm, at="rotateX", v=arm_back_swing/2, t=current_time)
+    mc.setKeyframe(right_leg, at="rotateX", v=leg_back_swing, t=current_time)
+    mc.setKeyframe(lego_body, at="rotateY", v=body_rotation, t=current_time)
+    mc.setKeyframe(lego_body, at="rotateX", v=body_sway, t=current_time)
 
     # Set keyframe swing leg forward
-    mc.rotate(-110, 0, 0, r=True)
-    mc.setKeyframe(at="rotateX", time=swing_kick_time)
+    current_time = start_time + (0.35*animation_t)
+    mc.setKeyframe(left_arm, at="rotateX", v=arm_swing, t=current_time)
+    mc.setKeyframe(right_arm, at="rotateX", v=arm_swing/2, t=current_time)
+    mc.setKeyframe(right_leg, at="rotateX", v=leg_swing, t=current_time)
+    mc.setKeyframe(lego_body, at="rotateY", v=-body_rotation, t=current_time)
+    mc.setKeyframe(lego_body, at="rotateX", v=-body_sway, t=current_time)
 
-    # Set keyframe leg coming to rest
-    mc.rotate(80, 0, 0, r=True)
-    mc.setKeyframe(at="rotateX", time=ending_time)
+    # Set end Keyframe
+    current_time = start_time + animation_t
+    mc.setKeyframe(left_arm, right_arm, right_leg, lego_body, at="rotateX", v=0, t=current_time)
+    mc.setKeyframe(lego_body, at="rotateY", v=0, t=current_time)
+    mc.currentTime(current_time)
 
-kick(mc.select())
+kick()
